bca93e7e62cd2a4ad2ee2965273768d708d4822a
David G <davidgust99@gmail.com>
2022-02-01 18:22:13 +0100

    Snackbar after saving new watch

--- src/app/components/new-watch-dialog/new-watch-dialog.component.ts
+++ src/app/components/new-watch-dialog/new-watch-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MainNavComponent } from '../main-nav/main-nav.component';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-new-watch-dialog',
@@ -8,11 +9,34 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./new-watch-dialog.component.scss'],
 })
 export class NewWatchDialogComponent implements OnInit {
-  constructor(public dialogRef: MatDialogRef<MainNavComponent>) {}
+  constructor(
+    public dialogRef: MatDialogRef<MainNavComponent>,
+    private _snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {}
 
+  saveWatch(): void {
+    // TODO: save new watch.
+    // If successfull: show success snackbar.
+    // Else: show unsuccessful snackbar
+    this.showSnackbar('Saved watch: Label');
+  }
+
   onCancelClick(): void {
     this.dialogRef.close();
+  }
+
+  showSnackbar(saveMessage: string, action?: string): void {
+    let snack = this._snackBar.open(saveMessage, action, {
+      panelClass: ['mat-toolbar', 'mat-primary'],
+      duration: 5000,
+    });
+    snack.afterDismissed().subscribe(() => {
+      console.log('This will be shown after snackbar disappeared');
+    });
+    snack.onAction().subscribe(() => {
+      console.log('This will be called when snackbar button clicked');
+    });
   }
 }
