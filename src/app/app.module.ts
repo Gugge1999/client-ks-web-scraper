ffab0adaa23a09d8819aef7b0bc3d83b5faeb436
David G <davidgust99@gmail.com>
2022-02-01 17:13:54 +0100

    Moved ng Material components to material.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { FormsModule } from '@angular/forms';
-
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 import { MainNavComponent } from './components/main-nav/main-nav.component';
@@ -36,16 +26,7 @@ import { DeleteWatchDialogComponent } from './components/delete-watch-dialog/del
     BrowserAnimationsModule,
     LayoutModule,
     FormsModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatInputModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatCardModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatSnackBarModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/material.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+const MaterialComponents = [
+  MatIconModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatListModule,
+  MatCardModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatSnackBarModule,
+];
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents],
+})
+export class MaterialModule {}
