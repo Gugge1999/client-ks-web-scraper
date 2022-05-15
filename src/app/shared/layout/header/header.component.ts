99ef11927f698f17fc943bcb9798e232b7559b8e
David G <davidgust99@gmail.com>
2022-05-15 17:51:08 +0200

    First commit on dark and light theme

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { ApiStatusDialogComponent } from '@components/api-status-dialog/api-status-dialog.component';
 import { ApiStatus } from '@models/api-status.model';
 import { StatusService } from '@shared/services/utils/status.service';
+import { ThemeService } from '@shared/services/utils/theme.service';
 
 @Component({
   selector: 'app-header',
@@ -11,11 +12,16 @@ import { StatusService } from '@shared/services/utils/status.service';
 })
 export class HeaderComponent implements OnInit {
   apiStatus!: ApiStatus;
+  isDarkMode: boolean = true;
 
   constructor(
     private dialog: MatDialog,
-    private statusService: StatusService
-  ) {}
+    private statusService: StatusService,
+    private themeService: ThemeService
+  ) {
+    this.themeService.initTheme();
+    this.isDarkMode = this.themeService.isDarkMode();
+  }
   ngOnInit(): void {
     this.statusService.getApiStatus().subscribe((res) => {
       this.apiStatus = res;
@@ -31,5 +37,12 @@ export class HeaderComponent implements OnInit {
       width: '450px',
       data: this.apiStatus,
     });
+  }
+
+  toggleTheme() {
+    this.isDarkMode = this.themeService.isDarkMode();
+    this.isDarkMode
+      ? this.themeService.updateTheme('light-mode')
+      : this.themeService.updateTheme('dark-mode');
   }
 }
---
+++ src/app/shared/services/utils/theme.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private _renderer!: Renderer2;
+  private _colorTheme!: string;
+
+  // Från: https://www.youtube.com/watch?v=XIUv27nYcLE
+  constructor(rendererFactory: RendererFactory2) {
+    this._renderer = rendererFactory.createRenderer(null, null);
+  }
+
+  initTheme() {
+    this.colorTheme;
+    this._renderer.addClass(document.body, this._colorTheme);
+  }
+
+  updateTheme(theme: 'dark-mode' | 'light-mode') {
+    this.colorTheme = theme;
+
+    const previousColorTheme =
+      theme === 'dark-mode' ? 'light-mode' : 'dark-mode';
+    this._renderer.removeClass(document.body, previousColorTheme);
+    this._renderer.addClass(document.body, theme);
+  }
+
+  isDarkMode(): boolean {
+    return this._colorTheme === 'dark-mode';
+  }
+
+  private set colorTheme(theme: string) {
+    this._colorTheme = theme;
+    localStorage.setItem('user-theme', theme);
+  }
+
+  private get colorTheme(): string {
+    if (localStorage.getItem('user-theme')) {
+      return (this.colorTheme =
+        localStorage.getItem('user-theme') ?? 'dark-mode');
+    } else {
+      return (this.colorTheme = 'dark-mode');
+    }
+  }
+}
