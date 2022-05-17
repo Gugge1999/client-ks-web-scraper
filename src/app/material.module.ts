0cf1390c062ddec09677a7c887fa060896437632
David G <davidgust99@gmail.com>
2022-05-17 21:36:20 +0200

    Hamburger menu

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {
@@ -29,6 +30,7 @@ const MaterialComponents = [
   MatSnackBarModule,
   MatToolbarModule,
   MatTooltipModule,
+  MatMenuModule,
 ];
 
 const matSnackbarDefaultConfig: MatSnackBarConfig = {
--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,3 +1,4 @@
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ApiStatusDialogComponent } from '@components/api-status-dialog/api-status-dialog.component';
@@ -13,16 +14,25 @@ import { ThemeService } from '@shared/services/utils/theme.service';
 export class HeaderComponent implements OnInit {
   apiStatus!: ApiStatus;
   isDarkMode: boolean = true;
+  showHamburgerMenu: boolean = true;
 
   constructor(
     private dialog: MatDialog,
     private statusService: StatusService,
-    private themeService: ThemeService
+    private themeService: ThemeService,
+    private breakpointObserver: BreakpointObserver
   ) {
     this.themeService.initTheme();
     this.isDarkMode = this.themeService.isDarkMode();
   }
   ngOnInit(): void {
+    this.breakpointObserver
+      .observe(['(min-width: 1000px)'])
+      .subscribe((state: BreakpointState) => {
+        state.matches
+          ? (this.showHamburgerMenu = false)
+          : (this.showHamburgerMenu = true);
+      });
     this.statusService.getApiStatus().subscribe((res) => {
       this.apiStatus = res;
     });
