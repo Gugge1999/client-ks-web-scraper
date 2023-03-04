2976861a3e44759abebdfa2449ea45f2d321ed76
David G <davidgust99@gmail.com>
2023-03-04 14:18:00 +0100

    Add firebase and Google Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
+import { filter } from 'rxjs/operators';
+
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { loadWatches } from '@store/actions/watch-api.actions';
+
+declare const gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -9,11 +14,25 @@ import { loadWatches } from '@store/actions/watch-api.actions';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private store: Store) {}
+  constructor(private store: Store, private router: Router) {}
 
   ngOnInit() {
+    this.setUpAnalytics();
+
     sessionStorage.removeItem('firstApiError');
 
     this.store.dispatch(loadWatches());
+  }
+
+  setUpAnalytics() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          gtag('config', 'G-2M7YJWSQ0F', {
+            page_path: event.urlAfterRedirects,
+          });
+        }
+      });
   }
 }
