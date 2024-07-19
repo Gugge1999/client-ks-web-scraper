2849bcd6b098bc6bdf7403ced73a3806f35cc731
David G <davidgust99@gmail.com>
2024-07-19 16:16:03 +0200

    Förbättra snack bar service

--- src/app/services/snack-bar.service.ts
+++ src/app/services/snack-bar.service.ts
@@ -1,27 +1,23 @@
 import { Injectable, inject } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
 
-@Injectable({
-  providedIn: "root",
-})
+@Injectable({ providedIn: "root" })
 export class SnackBarService {
   private readonly snackBar = inject(MatSnackBar);
   private readonly dismissMessage = "Avvisa";
-  private readonly undoMessage = "Ångra";
   private readonly snackBarClassConst = "snack-bar";
   private readonly successConst = "success";
   private readonly warningConst = "warning";
 
-  // TODO: Det kanske går att slå ihop alla dessa till endast en funktion
-  successSnackBar = (message: string) => {
+  successSnackBar(message: string) {
     return this.snackBar.open(message, this.dismissMessage, { panelClass: `${this.snackBarClassConst}-${this.successConst}` });
-  };
+  }
 
-  errorSnackBar = (message: string) => {
+  errorSnackBar(message: string) {
     return this.snackBar.open(`Error: ${message}`, this.dismissMessage, { panelClass: `${this.snackBarClassConst}-${this.warningConst}` });
-  };
+  }
 
-  undoSnackBar = (message: string) => {
-    return this.snackBar.open(message, this.undoMessage, { panelClass: `${this.snackBarClassConst}-${this.warningConst}` });
-  };
+  undoSnackBar(message: string) {
+    return this.snackBar.open(message, "Ångra", { panelClass: `${this.snackBarClassConst}-${this.warningConst}` });
+  }
 }
