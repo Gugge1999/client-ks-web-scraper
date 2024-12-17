c3cf18024e2aad19bbd547e74684068d2889c478
David <davidgust99@gmail.com>
2024-12-17 17:43:28 +0100

    Mer labbande med diagram över minnesanvändning

--- src/app/components/dialogs/api-status-dialog/api-status-dialog.component.ts
+++ src/app/components/dialogs/api-status-dialog/api-status-dialog.component.ts
@@ -13,20 +13,21 @@ import { TuiDialogContext, TuiPoint } from "@taiga-ui/core";
 import { injectContext } from "@taiga-ui/polymorpheus";
 import { TuiAxes, TuiLineChart, TuiLineChartHint } from "@taiga-ui/addon-charts";
 import { TuiContext } from "@taiga-ui/cdk";
+import { JsonPipe } from "@angular/common";
 
 @Component({
   selector: "scraper-api-status-dialog",
   templateUrl: "./api-status-dialog.component.html",
   styleUrls: ["./api-status-dialog.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [TuiAxes, TuiLineChart, TuiLineChartHint],
+  imports: [TuiAxes, TuiLineChart, TuiLineChartHint, JsonPipe],
 })
 export class ApiStatusDialogComponent implements OnInit {
   private readonly cdr = inject(ChangeDetectorRef);
 
-  public readonly context = injectContext<TuiDialogContext<void, InputSignal<ApiStatus>>>();
+  private chartChild = viewChild.required<TuiLineChart>("vette");
 
-  header = viewChild(TuiLineChart);
+  readonly context = injectContext<TuiDialogContext<void, InputSignal<ApiStatus>>>();
 
   readonly apiStatus = this.context.data;
   private readonly memoryUsageArr: TuiPoint[] = [];
@@ -35,13 +36,13 @@ export class ApiStatusDialogComponent implements OnInit {
 
   readonly hejsan = computed(() => {
     const memoryUsage = this.apiStatus().memoryUsage;
-    this.showChart = false;
+    // this.showChart = false;
 
     if (this.memoryUsageArr.length <= 9) {
       this.memoryUsageLength++;
 
       this.memoryUsageArr.push([this.memoryUsageArr.length, memoryUsage]);
-      this.showChart = true;
+      // this.showChart = true;
 
       return this.memoryUsageArr;
     }
@@ -53,7 +54,7 @@ export class ApiStatusDialogComponent implements OnInit {
 
     this.memoryUsageArr.push([this.memoryUsageLength, memoryUsage]);
 
-    this.showChart = true;
+    // this.showChart = true;
 
     return this.memoryUsageArr;
   });
@@ -73,7 +74,7 @@ export class ApiStatusDialogComponent implements OnInit {
 
   readonly stringify = String;
 
-  readonly axisXLabels: string[] = this.value.map(e => e[0].toString()).slice(1);
+  axisXLabels: string[] = this.value.map(e => e[0].toString()).slice(1);
 
   readonly axisXLabelsSig = computed(() =>
     this.hejsan()
@@ -89,7 +90,7 @@ export class ApiStatusDialogComponent implements OnInit {
         .reduce((a, b) => a + b) / this.hejsan().length,
   );
 
-  readonly axisYLabels: readonly string[] = [
+  axisYLabels: readonly string[] = [
     "0",
     (this.averageMemoryUsage * 0.5).toFixed(0).toString(), // Halva av medel
     this.averageMemoryUsage.toFixed(0).toString(), // Medel
@@ -107,6 +108,39 @@ export class ApiStatusDialogComponent implements OnInit {
 
   ngOnInit(): void {
     this.axisXLabels.push(this.value.length.toString());
+
+    let index = 10;
+
+    setInterval(() => {
+      this.showChart = false;
+      index++;
+      this.value.push([index, 123]);
+
+      this.axisXLabels = this.value.map(e => e[0].toString()).slice(1);
+      this.axisYLabels = [
+        "0",
+        (this.averageMemoryUsage * 0.5).toFixed(0).toString(), // Halva av medel
+        this.averageMemoryUsage.toFixed(0).toString(), // Medel
+        (this.averageMemoryUsage * 1.5).toFixed(0).toString(), // Halva över medel
+      ];
+
+      this.showChart = true;
+    }, 2_000);
+
+    // this.chartChild().value = [
+    //   [0, 100],
+    //   [1, 180],
+    //   [2, 50],
+    //   [3, 75],
+    //   [4, 50],
+    //   [5, 150],
+    //   [6, 175],
+    //   [7, 190],
+    //   [8, 70],
+    //   [9, 70],
+    // ];
+    //
+    // this.cdr.detectChanges();
   }
 
   readonly hintContent = ({ $implicit }: TuiContext<readonly TuiPoint[]>) => $implicit[0]?.[1] ?? 0;
