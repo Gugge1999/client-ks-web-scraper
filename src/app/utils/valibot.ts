68a718a611f2325622435ab8ac8eb1e0e2b4ce4f
David <davidgust99@gmail.com>
2025-02-12 20:12:29 +0100

    Byt från ! till === false

--- src/app/utils/valibot.ts
+++ src/app/utils/valibot.ts
@@ -1,11 +1,11 @@
 import { isDevMode } from "@angular/core";
-import { BaseSchema, BaseIssue, safeParse } from "valibot";
+import { BaseIssue, BaseSchema, safeParse } from "valibot";
 
 export function verifyResponse<T>(schema: BaseSchema<unknown, unknown, BaseIssue<unknown>>, response: T): void {
   if (isDevMode()) {
     const result = safeParse(schema, response);
 
-    if (!result.success) {
+    if (result.success === false) {
       console.error("valibot error", result.issues);
     }
   }
