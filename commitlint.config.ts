f85ccd407dac19002383beb8b0a3b1aee18bbe5f
David <davidgust99@gmail.com>
2025-03-17 13:28:59 +0100

    Radera commitlint

--- commitlint.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  extends: ["@commitlint/config-angular"],
-  rules: {
-    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "chore"]],
-    "scope-enum": [2, "always", ["flera af", "config"]],
-  },
-};
