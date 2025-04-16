118381209489824ed3079ca012cc2eaeeda01592
David <david.gustafsson@cgi.com>
2025-04-16 13:47:54 +0200

    Flytta in object till watchSchema

--- src/app/models/watch.model.ts
+++ src/app/models/watch.model.ts
@@ -1,11 +1,5 @@
 import { array, boolean, InferOutput, isoTimestamp, nullable, object, pipe, string, url, uuid } from "valibot";
 
-export const scrapedWatch = object({
-  name: string(),
-  postedDate: pipe(string(), isoTimestamp()),
-  link: pipe(string(), url()),
-});
-
 export const watchSchema = object({
   id: pipe(string(), uuid()),
   active: boolean(),
@@ -14,7 +8,11 @@ export const watchSchema = object({
   lastEmailSent: nullable(pipe(string(), isoTimestamp())),
   watchToScrape: pipe(string(), url()),
   notifications: array(pipe(string(), isoTimestamp())),
-  latestWatch: scrapedWatch,
+  latestWatch: object({
+    name: string(),
+    postedDate: pipe(string(), isoTimestamp()),
+    link: pipe(string(), url()),
+  }),
 });
 
 // OBS! Låt det vara ett interface. Om det är "type" kommer alla props att expanderas vid hover istället för att visa "Watch"
