affffb38c791503759008bb1944ead6c95806df8
David <davidgust99@gmail.com>
2025-07-20 18:14:11 +0200

    Bryt ut css till egen fil

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
 @Component({
   selector: "scraper-footer",
   templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent {}
